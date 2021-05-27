e9e3c6d7a48dbd195798bc47522e853614d48e1e
Grinch101 <Masoud.Kameli@gmail.com>
2021-05-27 23:30:08 +0430

    activity router added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const user_router = require("./routes/user_router");
 const contact_router = require('./routes/contact_router')
+const activity_router = require('./routes/activity_router')
 const get_client = require("./middleware/get_conn");
 const httpError = require('./middleware/httpError');
 const my_hstore = require('./middleware/my_hstore');
@@ -12,7 +13,7 @@ app.use(my_hstore);
 app.use(get_client);
 app.use("/auth", user_router);
 app.use("/contact", contact_router);
-// app.use('/activity', activity_router);
+app.use('/activity', activity_router);
 
 
 app.use(httpError)
